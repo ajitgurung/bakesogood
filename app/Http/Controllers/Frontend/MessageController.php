6d56fd58a31d4fa2463e5378fb683ada41e54527
ajitgurung <ajtgurung04@gmail.com>
2024-11-07 19:38:56 -0500

    http error fixed

--- app/Http/Controllers/Frontend/MessageController.php
+++ app/Http/Controllers/Frontend/MessageController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Mail\ContactFormMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
